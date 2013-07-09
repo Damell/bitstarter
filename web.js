72e8c52805812836c1b082d9c9adbc65eb2c680e
Daniel Chabr <danielchabr@gmail.com>
2013-07-09 15:54:31 +0000

    Added index.html v2

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
+var fs = require('fs');
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var welcome_text = fs.readFileSync("index.html", "utf-8");
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(welcome_text);
 });
 
 var port = process.env.PORT || 5000;
